62946bbd4c8766ddae22d3d82801c0d1772c6e1d
bochain <53370227+cantpr09ram@users.noreply.github.com>
2024-04-23 23:34:55 +0800

    Initial Commit

---
+++ gandizhen/gandizhenApp.swift
@@ -0,0 +1,17 @@
+//
+//  gandizhenApp.swift
+//  gandizhen
+//
+//  Created by \u8b19 on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct gandizhenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
